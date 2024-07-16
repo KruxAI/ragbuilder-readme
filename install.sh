b3ef7edfcce8bbc145014787fd30b3d010158467
ashwinzyx <167959062+ashwinzyx@users.noreply.github.com>
2024-07-16 19:10:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,24 @@ curl -fsSL https://raw.githubusercontent.com/KruxAI/ragbuilder-readme/main/Brewf
 # Install Homebrew packages using Brewfile
 echo "Installing Homebrew packages..."
 brew bundle install --file=Brewfile
- 
+
+
+# Proceed with your Python package setup
+VENV_PATH="./venv"
+
+echo "Creating Python virtual environment in the current folder at $VENV_PATH..."
+python3 -m venv $VENV_PATH
+
+# Debugging: Check Python interpreter and venv path
+echo "Current Python interpreter: $(which python3)"
+echo "VENV_PATH: $VENV_PATH"
+
+# Debugging: Check if venv is activated
+echo "Virtual environment activated: $(which python)"
+
 echo "Installing ragbuilder..."
 python3 -m pip install ragbuilder
 
+echo "Setup completed successfully."
+source $VENV_PATH/bin/activate
+ 
