d7a09b63825f521f09c723139a6252060e47767c
ashwinzyx <167959062+ashwinzyx@users.noreply.github.com>
2024-07-20 19:44:14 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,33 +1,17 @@
-@echo off
+echo Installing RagBuilder
 setlocal EnableDelayedExpansion
 
-:: Exit immediately if a command exits with a non-zero status.
-set "ERRORLEVEL=0"
-
-:: Function to check error level and exit if non-zero
-:checkErrorLevel
-if not "%ERRORLEVEL%" == "0" (
-    echo Error: Command failed with exit code %ERRORLEVEL%
-    exit /b %ERRORLEVEL%
-)
-goto :eof
-
-:: Download the Brewfile
 echo Downloading Brewfile...
 curl -fsSL https://raw.githubusercontent.com/KruxAI/ragbuilder-readme/main/Brewfile -o Brewfile
-call :checkErrorLevel
 
-:: Read Brewfile and install packages
+echo Reading Brewfile...
 echo Installing packages from Brewfile...
 for /f "usebackq tokens=*" %%i in ("Brewfile") do (
     python -m pip install %%i
-    call :checkErrorLevel
 )
 
-:: Install ragbuilder
 echo Installing ragbuilder...
 python -m pip install ragbuilder
-call :checkErrorLevel
 
 echo Setup completed successfully.
 endlocal
